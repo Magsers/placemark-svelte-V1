a5980cff0a5de2479fc8a922254f9fd83fe6552d
Margaret McCarthy <76971580+Magsers@users.noreply.github.com>
2022-06-09 10:04:38 +0100

    Update AddRouteForm.svelte

--- src/components/AddRouteForm.svelte
+++ src/components/AddRouteForm.svelte
@@ -43,7 +43,7 @@
         name: routeClimbed,
         grade: selectedGrade,
         height: height,
-        description: description,
+        // description: description,
         crag: crag._id,
         lat: lat,
         lng: lng,
